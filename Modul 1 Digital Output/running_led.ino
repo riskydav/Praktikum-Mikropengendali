373dd19b40235e2481190f3893ba38a9eee92eb7
Risky David K <89583633+riskydav@users.noreply.github.com>
2021-11-17 22:04:16 +0700

    Update running_led.ino

--- Modul 1 Digital Output/running_led.ino
+++ Modul 1 Digital Output/running_led.ino
@@ -1,35 +1,22 @@
-//instalasi Pin LED
-//Pin 8 untuk LED 
-int LED1 = 8;
-int LED2 = 7;
-int LED3 = 9;
-int LED4 = 10;
+int LED;
+int waktu = 200;
 
 void setup()
 {
-  //LED sebagai output
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-}
-
+  for (LED = 8; LED <= 11; LED++)
+  {
+    pinMode(LED, OUTPUT);
+    digitalWrite (LED, LOW);
+  }
+  
+  
 void loop()
 {
-  digitalWrite(LED1, HIGH);
-  delay(1000);
-  digitalWrite(LED1, LOW);
-  delay(1000);
-  digitalWrite(LED2, HIGH);
-  delay(1000);
-  digitalWrite(LED2, LOW);
-  delay(1000);
-  digitalWrite(LED3, HIGH);
-  delay(1000);
-  digitalWrite(LED3, LOW);
-  delay(1000);
-  digitalWrite(LED4, HIGH);
-  delay(1000);
-  digitalWrite(LED4, LOW);
-  delay(1000);
+  for (LED = 8; LED <= 11; LED++)
+  {
+    digitalWrite(LED, HIGH);
+    delay(waktu);
+    digitalWrite(LED, LOW);
+    
+  }
 }
